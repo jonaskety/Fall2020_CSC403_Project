7fca019aba2da9d7d15929e43d130ec8bbfadd20
codyjohnson2836 <37808393+codyjohnson2836@users.noreply.github.com>
2020-11-12 08:16:25 -0600

    Added layout for inventory

--- Project/Fall2020_CSC403_Project/FrmInventory.Designer.cs
+++ Project/Fall2020_CSC403_Project/FrmInventory.Designer.cs
@@ -29,20 +29,177 @@ namespace Fall2020_CSC403_Project
         /// </summary>
         private void InitializeComponent()
         {
+            this.picPlayer = new System.Windows.Forms.PictureBox();
+            this.picItem1 = new System.Windows.Forms.PictureBox();
+            this.picItem2 = new System.Windows.Forms.PictureBox();
+            this.picItem3 = new System.Windows.Forms.PictureBox();
+            this.picItem4 = new System.Windows.Forms.PictureBox();
+            this.picItem5 = new System.Windows.Forms.PictureBox();
+            this.picItem6 = new System.Windows.Forms.PictureBox();
+            this.picItem7 = new System.Windows.Forms.PictureBox();
+            this.picItem8 = new System.Windows.Forms.PictureBox();
+            this.picItem9 = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem9)).BeginInit();
             this.SuspendLayout();
+            // 
+            // picPlayer
+            // 
+            this.picPlayer.BackgroundImage = global::Fall2020_CSC403_Project.Properties.Resources.player;
+            this.picPlayer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.picPlayer.Location = new System.Drawing.Point(99, 68);
+            this.picPlayer.Name = "picPlayer";
+            this.picPlayer.Size = new System.Drawing.Size(122, 250);
+            this.picPlayer.TabIndex = 0;
+            this.picPlayer.TabStop = false;
+            // 
+            // picItem1
+            // 
+            this.picItem1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.picItem1.Location = new System.Drawing.Point(323, 114);
+            this.picItem1.Name = "picItem1";
+            this.picItem1.Size = new System.Drawing.Size(100, 100);
+            this.picItem1.TabIndex = 1;
+            this.picItem1.TabStop = false;
+            // 
+            // picItem2
+            // 
+            this.picItem2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.picItem2.Location = new System.Drawing.Point(443, 114);
+            this.picItem2.Name = "picItem2";
+            this.picItem2.Size = new System.Drawing.Size(100, 100);
+            this.picItem2.TabIndex = 2;
+            this.picItem2.TabStop = false;
+            // 
+            // picItem3
+            // 
+            this.picItem3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.picItem3.Location = new System.Drawing.Point(564, 114);
+            this.picItem3.Name = "picItem3";
+            this.picItem3.Size = new System.Drawing.Size(100, 100);
+            this.picItem3.TabIndex = 3;
+            this.picItem3.TabStop = false;
+            // 
+            // picItem4
+            // 
+            this.picItem4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.picItem4.Location = new System.Drawing.Point(323, 235);
+            this.picItem4.Name = "picItem4";
+            this.picItem4.Size = new System.Drawing.Size(100, 100);
+            this.picItem4.TabIndex = 4;
+            this.picItem4.TabStop = false;
+            // 
+            // picItem5
+            // 
+            this.picItem5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.picItem5.Location = new System.Drawing.Point(443, 235);
+            this.picItem5.Name = "picItem5";
+            this.picItem5.Size = new System.Drawing.Size(100, 100);
+            this.picItem5.TabIndex = 5;
+            this.picItem5.TabStop = false;
+            // 
+            // picItem6
+            // 
+            this.picItem6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.picItem6.Location = new System.Drawing.Point(564, 235);
+            this.picItem6.Name = "picItem6";
+            this.picItem6.Size = new System.Drawing.Size(100, 100);
+            this.picItem6.TabIndex = 6;
+            this.picItem6.TabStop = false;
+            // 
+            // picItem7
+            // 
+            this.picItem7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.picItem7.Location = new System.Drawing.Point(323, 357);
+            this.picItem7.Name = "picItem7";
+            this.picItem7.Size = new System.Drawing.Size(100, 100);
+            this.picItem7.TabIndex = 7;
+            this.picItem7.TabStop = false;
+            // 
+            // picItem8
+            // 
+            this.picItem8.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.picItem8.Location = new System.Drawing.Point(443, 357);
+            this.picItem8.Name = "picItem8";
+            this.picItem8.Size = new System.Drawing.Size(100, 100);
+            this.picItem8.TabIndex = 8;
+            this.picItem8.TabStop = false;
+            // 
+            // picItem9
+            // 
+            this.picItem9.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.picItem9.Location = new System.Drawing.Point(564, 357);
+            this.picItem9.Name = "picItem9";
+            this.picItem9.Size = new System.Drawing.Size(100, 100);
+            this.picItem9.TabIndex = 9;
+            this.picItem9.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(51, 377);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(220, 111);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit Inventory";
+            this.btnExit.UseVisualStyleBackColor = false;
             // 
             // FrmInventory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(714, 574);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.picItem9);
+            this.Controls.Add(this.picItem8);
+            this.Controls.Add(this.picItem7);
+            this.Controls.Add(this.picItem6);
+            this.Controls.Add(this.picItem5);
+            this.Controls.Add(this.picItem4);
+            this.Controls.Add(this.picItem3);
+            this.Controls.Add(this.picItem2);
+            this.Controls.Add(this.picItem1);
+            this.Controls.Add(this.picPlayer);
             this.Name = "FrmInventory";
             this.Text = "Inventory";
             this.Load += new System.EventHandler(this.FrmInventory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picItem9)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picPlayer;
+        private System.Windows.Forms.PictureBox picItem1;
+        private System.Windows.Forms.PictureBox picItem2;
+        private System.Windows.Forms.PictureBox picItem3;
+        private System.Windows.Forms.PictureBox picItem4;
+        private System.Windows.Forms.PictureBox picItem5;
+        private System.Windows.Forms.PictureBox picItem6;
+        private System.Windows.Forms.PictureBox picItem7;
+        private System.Windows.Forms.PictureBox picItem8;
+        private System.Windows.Forms.PictureBox picItem9;
+        private System.Windows.Forms.Button btnExit;
     }
 }
