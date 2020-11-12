3076312ff19503eb95aa6bd6cc5e03466551f826
FrankieCook <nfcook097@gmail.com>
2020-11-11 19:59:35 -0600

    Baby Peanut

--- Project/MyGameLibrary/Player.cs
+++ Project/MyGameLibrary/Player.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Fall2020_CSC403_Project.code {
   public class Player : BattleCharacter {
+    /// <summary>
+    /// THis is the image for the player
+    /// </summary>
+    public Image Img { get; set; }
+
     public Player(Vector2 initPos, Collider collider) : base(initPos, collider) {
 
     }
