551e2b2a3c487eaa3913bbf707cfe443282f0641
codyjohnson2836 <37808393+codyjohnson2836@users.noreply.github.com>
2020-11-12 22:29:47 -0600

    Part 1 of upload
    Loot updates properly. Waiting until health system/strength system is in place before I actually modify any damage values.

---
+++ Project/MyGameLibrary/Food.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2020_CSC403_Project.code
+{
+    public class Food : Item
+    {
+        public Food(Image img, int pwr, int cst, char tpe)
+        {
+            image = img;
+            power = pwr;
+            cost = cst;
+            type = tpe;
+        }
+
+        public Image image { get; set; }
+        public int power { get; set; }
+        public int cost { get; set; }
+        public char type { get; set; }
+
+        public void Use()
+        {
+
+        }
+    }
+}
---
+++ Project/MyGameLibrary/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2020_CSC403_Project.code
+{
+    public interface Item
+    {
+        Image image { get; set; }
+        int power { get; set; }
+        int cost { get; set; }
+        char type { get; set; }
+
+        void Use();
+    }
+}
